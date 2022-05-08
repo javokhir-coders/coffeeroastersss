69f90d6bf4116e42930d933d3ea51fa2c5d18b6b
javokhircoders <javohirruzimurotov3@gmail.com>
2022-05-09 03:31:31 +0500

    initial commit

---
+++ js/about.js
@@ -0,0 +1,9 @@
+// var elButton = document.querySelector(".collection__box1__button");
+// var elContent = document.querySelector(".collection__box1__content")
+
+// elButton.addEventListener("click", function(){
+//   elContent.classList.toggle("collection__box1__content--open")
+// })
+
+
+
---
+++ js/main2.js
@@ -0,0 +1,6 @@
+// var elButton = document.querySelector(".collection__box2__button");
+// var elContent = document.querySelector(".collection__box2__content")
+
+// elButton.addEventListener("click", function(){
+//   elContent.classList.toggle("collection__box2__content--open")
+// })
